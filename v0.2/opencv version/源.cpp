4d41bf33bf90ba3b6991841933ce2374401f3752
lishijm <lishijm@outlook.com>
2019-07-27 22:33:01 +0800

    霍夫变换参数调整，边缘检测改为canny算子

--- v0.2/opencv version/源.cpp
+++ v0.2/opencv version/源.cpp
@@ -26,12 +26,12 @@ Mat gryi(Mat ogimg) {
 }
 //prewitt算子边缘检测
 Mat prewitt(Mat ogimg) {
-	float prewittx[9] ={
+	float prewittx[9] = {
 		-1,0,1,
 		-1,0,1,
 		-1,0,1
 	};
-	float prewitty[9] ={
+	float prewitty[9] = {
 		1,1,1,
 		0,0,0,
 		-1,-1,-1
@@ -46,8 +46,8 @@ Mat prewitt(Mat ogimg) {
 	filter2D(ogimg, dstx, ogimg.depth(), px);
 	filter2D(ogimg, dsty, ogimg.depth(), py);
 	float tempx, tempy, temp;
-	for (int i = 0; i < ogimg.rows; i++){
-		for (int j = 0; j < ogimg.cols; j++){
+	for (int i = 0; i < ogimg.rows; i++) {
+		for (int j = 0; j < ogimg.cols; j++) {
 			tempx = dstx.at<uchar>(i, j);
 			tempy = dsty.at<uchar>(i, j);
 			temp = sqrt(tempx * tempx + tempy * tempy);
@@ -61,48 +61,50 @@ Mat dilatei(Mat ogimg) {
 	Mat dst;
 	Mat structure_element = getStructuringElement(MORPH_RECT, Size(3, 3)); //设置膨胀/腐蚀的核为矩形，大小为3*3
 	dilate(ogimg, dst, structure_element); //膨胀
-	erode(dst, dst, structure_element);//腐蚀
+	//erode(dst, dst, structure_element);//腐蚀
 	return dst;
 }
 //霍夫变换
-Mat houghi(Mat ogimg,Mat ogoimg) {
-	/*vector<Vec4f> line_data;
-	HoughLinesP(ogimg, line_data, 1, CV_PI / 180.0, 80, 10, 20);
-	//把得到的直线显示在图中
-	Scalar color = Scalar(255, 0, 0);
-	for (size_t i = 0; i < line_data.size(); i++) {
-		Vec4f temp = line_data[i];
-		line(ogoimg, Point(temp[0], temp[1]), Point(temp[2], temp[3]), color, 2);
-	}
-	return ogoimg;*/
-	vector<Vec2f> lines;
-	HoughLines(ogimg, lines, 1, CV_PI / 5, 10);
-	std::vector<Vec2f>::const_iterator it = lines.begin();
-	while (it != lines.end())
+Mat houghi(Mat ogimg, Mat ogoimg) {
+
+	vector<Vec4i>lines;
+	HoughLinesP(ogimg, lines, 1, CV_PI / 180, 180, 50, 10);
+
+	//一次在图中绘制出每条线段
+	for (size_t i = 0; i < lines.size(); i++)
 	{
-		float rho = (*it)[0];
-		float theta = (*it)[1];
-		if (theta < CV_PI / 4 || theta > 3 * CV_PI / 4){
-			//画垂直线
-			Point pt1(rho / cos(theta), 0);
-			Point pt2((rho - ogimg.rows * sin(theta)) / cos(theta), ogimg.rows);
-			line(ogoimg, pt1, pt2, Scalar(0, 0, 255), 1, LINE_AA);
-		}
-		else{
-			//画水平线
-			Point pt1(0, rho / sin(theta));
-			Point pt2(ogimg.cols, (rho - ogimg.cols * cos(theta)) / sin(theta));
-			line(ogoimg, pt1, pt2, Scalar(0, 0, 255), 1, LINE_AA);
-		}
-		++it;
-	}
+		Vec4i l = lines[i];
+		line(ogoimg, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(186, 88, 255), 1, LINE_AA);
+
+	}/*
+	vector<Vec2f>lines;
+	HoughLines(ogimg, lines, 1, CV_PI / 180, 150, 0, 0);
+	for (size_t i = 0; i < lines.size(); i++){
+		float rho = lines[i][0];
+		float theta = lines[i][1];
+		Point pt1, pt2;
+		double a = cos(theta), b = sin(theta);
+		double x0 = a * rho, y0 = b * rho;
+		pt1.x = cvRound(x0 + 1000 * (-b));
+		pt1.y = cvRound(y0 + 1000 * (a));
+		pt2.x = cvRound(x0 - 1000 * (-b));
+		pt2.y = cvRound(y0 - 1000 * (a));
+		line(ogoimg, pt1, pt2, Scalar(55, 100, 195), 1, LINE_AA);
+
+	}*/
 	return ogoimg;
 }
+Mat cannyi(Mat ogimg) {
+	Mat dst, dst2;
+	Canny(ogimg, dst, 50, 200, 3);
+	cvtColor(dst, dst2, CV_GRAY2BGR);
+	return dst;
+}
 
-int main(){
+int main() {
 	//读取图像
 	Mat img = imread("D:\\opencvtest\\03.jpg");
-	if (img.empty()){
+	if (img.empty()) {
 		cout << "fail to load image !" << endl;
 		return -1;
 	}
@@ -116,14 +118,17 @@ int main(){
 	equalizeHist(mbimg, blimg);
 	//边缘检测
 	Mat ptimg = prewitt(blimg);
+	Mat canimg = cannyi(blimg);
 	//膨胀后腐蚀
 	Mat dilateimg = dilatei(ptimg);
+	Mat dilateimg2 = dilatei(canimg);
 	//hough变换检测直线
-	Mat houimg = houghi(dilateimg,img);
+	Mat houimg = houghi(dilateimg2, img);
 	//显示
 	namedWindow("opencv test", CV_WINDOW_AUTOSIZE);
 	imshow("opencv test", houimg);
 	imshow("imgorg", gryimg);
+	imshow("canimg", dilateimg2);
 	waitKey(0);
 	return 0;
 }
